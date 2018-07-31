463512a4f5f5d69295dbec51d8b4aea60e215cad
NaarGes <narges.sadeghi73@gmail.com>
2018-07-31 11:54:12 +0430

    Navigation drawer in Main activity, show toast on selected items.

---
+++ app/src/main/java/com/example/asus/yarafirstproject/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.asus.yarafirstproject;
+
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dl = findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.profile_nd:
+                        Toast.makeText(MainActivity.this, "پروفایل کاربر",Toast.LENGTH_SHORT).show();
+                    case R.id.contact_nd:
+                        Toast.makeText(MainActivity.this, "درباره ما",Toast.LENGTH_SHORT).show();
+                    case R.id.about_nd:
+                        Toast.makeText(MainActivity.this, "تماس با ما",Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
